b8558d7dc89145bfaeb5b688b79a04139f6c6216
Matt Ellis <matt@pulumi.com>
2018-07-20 10:35:53 -0700

    Open source `hack-vendor`
    `hack-vendor` is a small script that makes it quick to override a
    vendored dependency (managed by `dep`) with a local version. It works
    by spinning up a local web server that exposes $GOPATH/src and then
    modifies the `Gopkg.toml` file of a project to override the source of
    the package you want to modify to point at this local server and does
    a restore.
    
    The web-server listens on port 80, due to limitiations with `dep` when
    this script was authored. It appears that newer versions would allow
    sources to actually have a non standard port, so in the future we may
    be able to modify the scripts to just have the web-server listen on
    some non standard port, which would prevent the need to sudo.

---
+++ hack-vendor/cmd/change-constraint/main.go
@@ -0,0 +1,120 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+)
+
+func main() {
+	var name string
+	var revision string
+	var serverPrefix string
+	var gopkgFile string
+
+	flag.StringVar(&name, "name", "", "the name of the project to modify")
+	flag.StringVar(&revision, "revision", "", "the revision of the project to pin to")
+	flag.StringVar(&serverPrefix, "serverPrefix", "http://localhost.pulumi.engineering/", "the url of a git server that exposes $GOPATH")
+	flag.StringVar(&gopkgFile, "file", "Gopkg.toml", "the path to the Gopkg.toml to modify")
+	flag.Parse()
+
+	if name == "" {
+		fmt.Fprintf(os.Stderr, "error: must provide package to modify with -name\n")
+		os.Exit(1)
+	}
+
+	if revision == "" {
+		fmt.Fprintf(os.Stderr, "error: must provide version to use with -version\n")
+		os.Exit(1)
+	}
+
+	if !strings.HasSuffix(serverPrefix, "/") {
+		serverPrefix = serverPrefix + "/"
+	}
+
+	b, err := ioutil.ReadFile(gopkgFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	var gopkg Gopkg
+
+	if err := toml.Unmarshal(b, &gopkg); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	hadExisting := false
+	for idx, override := range gopkg.Override {
+		if override.Name == name {
+			newSource := serverPrefix + override.Name
+
+			if override.Source != "" {
+				newSource = serverPrefix + override.Source
+			}
+
+			gopkg.Override[idx] = Override{
+				Name:     name,
+				Revision: revision,
+				Source:   newSource,
+			}
+
+			hadExisting = true
+			break
+		}
+	}
+
+	if !hadExisting {
+		gopkg.Override = append(gopkg.Override, Override{
+			Name:     name,
+			Revision: revision,
+			Source:   serverPrefix + name,
+		})
+	}
+
+	f, err := os.OpenFile(gopkgFile, os.O_RDWR|os.O_TRUNC, 0644)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	err = toml.NewEncoder(f).Encode(gopkg)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+type Gopkg struct {
+	Required []string   `toml:"required"`
+	Ignored  []string   `toml:"ignored"`
+	Override []Override `toml:"override"`
+}
+
+type Override struct {
+	Name     string `toml:"name"`
+	Version  string `toml:"version,omitempty"`
+	Branch   string `toml:"branch,omitempty"`
+	Revision string `toml:"revision,omitempty"`
+	Source   string `toml:"source,omitempty"`
+}
