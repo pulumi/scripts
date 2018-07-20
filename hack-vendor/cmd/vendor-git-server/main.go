90cf0cfa1e0f3b165d398fcf0f2d00936109c774
Matt Ellis <matt@pulumi.com>
2018-07-20 10:35:53 -0700

    Open source `hack-vendor`
    `hack-vendor` is a small script that makes it quick to override a
    vendored dependency (managed by `dep`) with a local version. It works
    by spinning up a local web server that exposes $GOPATH/src and then
    modifies the `Gopkg.toml` file of a project to override the source of
    the package you want to modify to point at this local server and does
    a restore.
    
    The web-server listens on port 80, due to limitiations with `dep` when
    this script was authored. It appears that newer versions would allow
    sources to actually have a non standard port, so in the future we may
    be able to modify the scripts to just have the web-server listen on
    some non standard port, which would prevent the need to sudo.

---
+++ hack-vendor/cmd/vendor-git-server/main.go
@@ -0,0 +1,65 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"net/http/cgi"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	var root string
+	flag.StringVar(&root, "root", "", "root directory to serve from")
+	flag.Parse()
+
+	gitPath, err := exec.LookPath("git")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	if root == "" {
+		fmt.Fprintf(os.Stderr, "error: must supply path with -root\n")
+		os.Exit(1)
+	}
+
+	handler := &cgi.Handler{
+		Path: gitPath,
+		Root: "",
+		Args: []string{"http-backend"},
+		Env: []string{
+			fmt.Sprintf("GIT_PROJECT_ROOT=%v", root),
+			"GIT_HTTP_EXPORT_ALL=1",
+		},
+	}
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
+		values := req.URL.Query()
+		if values.Get("go-get") == "1" {
+			rw.Write([]byte(fmt.Sprintf("<meta name=\"go-import\" content=\"localhost.pulumi.engineering git http://localhost.pulumi.engineering/%v\">", req.URL.Path)))
+		} else {
+			handler.ServeHTTP(rw, req)
+		}
+	})
+
+	if err := http.ListenAndServe(fmt.Sprintf("localhost:80"), nil); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
