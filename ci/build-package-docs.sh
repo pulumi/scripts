1da80573f61fb718a1aa363fb73c2550988302dd
Pat Gavlin <pat@pulumi.com>
2019-09-05 11:09:46 -0700

    Tabs -> spaces; make script executable

--- ci/build-package-docs.sh
+++ ci/build-package-docs.sh
@@ -5,18 +5,18 @@ set -o errexit
 set -o pipefail
 
 if [[ "${TRAVIS:-}" != "true" ]]; then
-	echo "error: this script should be run from within Travis"
-	exit 1
+    echo "error: this script should be run from within Travis"
+    exit 1
 fi
 
 if [[ -z "${1:-}" ]]; then
-	echo "usage $0 <package-simple-name>"
-	exit 1
+    echo "usage $0 <package-simple-name>"
+    exit 1
 fi
 
 if ! echo "${TRAVIS_TAG:-}" | grep -q -e "^v[0-9]\+\.[0-9]\+\.[0-9]\+$"; then
-	echo "Skipping documentation generation; ${TRAVIS_TAG:-} does not denote a released version"
-	exit 0
+    echo "Skipping documentation generation; ${TRAVIS_TAG:-} does not denote a released version"
+    exit 0
 fi
 
 PKG_NAME="$1"
@@ -40,15 +40,15 @@ PKGS=${PKG_NAME} NOBUILD=true ./scripts/run_typedoc.sh
 ./scripts/generate_python_docs.sh
 
 if [[ "${PKG_NAME}" == "pulumi" ]]; then
-	# Regenerate the CLI docs
-	pulumi gen-markdown ./content/docs/reference/cli
+    # Regenerate the CLI docs
+    pulumi gen-markdown ./content/docs/reference/cli
 
-	# Update latest-version
-	echo -n "${VERSION}" > ./static/latest-version
+    # Update latest-version
+    echo -n "${VERSION}" > ./static/latest-version
 
-	# Update the version list
-	NL=$'\n' 
-	sed -e "s/<tbody>/<tbody>\\${NL}        {{< changelog-table-row version=\"${VERSION}\" date=\"$(date +%Y-%m-%d)\" >}}/" -i ./content/docs/get-started/install/versions.md
+    # Update the version list
+    NL=$'\n'
+    sed -e "s/<tbody>/<tbody>\\${NL}        {{< changelog-table-row version=\"${VERSION}\" date=\"$(date +%Y-%m-%d)\" >}}/" -i ./content/docs/get-started/install/versions.md
 fi
 
 # Commit the resulting changes
@@ -64,16 +64,16 @@ git commit --allow-empty -m "${MESSAGE}"
 # If we have a token for pulumi-bot, push up the changes and add a status
 # to a github compare.
 if [ ! -z "${PULUMI_BOT_GITHUB_API_TOKEN:-}" ]; then
-	# Push up the resulting changes
-	git remote add pulumi-bot "https://pulumi-bot:${PULUMI_BOT_GITHUB_API_TOKEN}@github.com/pulumi-bot/docs"
-	git push pulumi-bot --set-upstream --force "${BRANCH}"
+    # Push up the resulting changes
+    git remote add pulumi-bot "https://pulumi-bot:${PULUMI_BOT_GITHUB_API_TOKEN}@github.com/pulumi-bot/docs"
+    git push pulumi-bot --set-upstream --force "${BRANCH}"
 
-	# Create a pull request in the docs repo.
-	BODY="{\"title\": \"${MESSAGE}\", \"head\": \"pulumi-bot:${BRANCH}\", \"base\": \"master\"}"
-	curl -u "pulumi-bot:${PULUMI_BOT_GITHUB_API_TOKEN}" -X POST -H "Content-Type: application/json" -d "${BODY}" "https://api.github.com/repos/pulumi/docs/pulls"
+    # Create a pull request in the docs repo.
+    BODY="{\"title\": \"${MESSAGE}\", \"head\": \"pulumi-bot:${BRANCH}\", \"base\": \"master\"}"
+    curl -u "pulumi-bot:${PULUMI_BOT_GITHUB_API_TOKEN}" -X POST -H "Content-Type: application/json" -d "${BODY}" "https://api.github.com/repos/pulumi/docs/pulls"
 else
-	# Otherwise, just print out the diff to the build log.
-	git diff HEAD~1 HEAD
+    # Otherwise, just print out the diff to the build log.
+    git diff HEAD~1 HEAD
 fi
 
 exit 0
