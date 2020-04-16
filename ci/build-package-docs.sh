0defe6eb5f8a7b327c070b46c6b312e35251c932
Chris Smith <chris@pulumi.com>
2020-04-15 22:54:17 -0700

    Filter out 'aiven', 'mailgun'

--- ci/build-package-docs.sh
+++ ci/build-package-docs.sh
@@ -64,7 +64,7 @@ case ${PKG_NAME} in
     "pulumi" | "policy")
         echo "Skipping gen_resource_docs step because package doesn't contain any resources."
         ;;
-    "awsx" | "eks" | "kubernetesx")
+    "aiven" | "awsx" | "eks" | "kubernetesx" | "mailgun")
         # gen_resource_docs.sh assumes the package has a `make generate_schema` step.
         echo "Skipping gen_resource_docs step because package hasn't been schematized yet."
         ;;
